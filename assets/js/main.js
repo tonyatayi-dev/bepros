b8b482bdb19a0c5e5e2db9eb301ba8d0a1a129ca
tonyatayi <aatayi@outlook.com>
2022-03-25 15:18:59 +0000

    final

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,3 +23,45 @@ $('.hero-slider').owlCarousel({
     //     }
     // }
 })
+
+// Porfolio Slider
+$('.portfolio-slider').owlCarousel({
+    loop:true,
+    nav:true,
+    dots: false,
+    smartSpeed: 1000,
+    margin: 24,
+    navText: ["<i class='ri-arrow-left-line'></i>", "<i class='ri-arrow-right-line'></i>"],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        992:{
+            items:3
+        }
+    }
+})
+
+// Reviews Slider
+$('.reviews-slider').owlCarousel({
+    loop:true,
+    nav:true,
+    dots: false,
+    smartSpeed: 1000,
+    margin: 24,
+    navText: ["<i class='ri-arrow-left-line'></i>", "<i class='ri-arrow-right-line'></i>"],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        992:{
+            items:2
+        }
+    }
+})
